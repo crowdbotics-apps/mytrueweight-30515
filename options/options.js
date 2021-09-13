be9f8bc8882cd31a29f4e13ed19cdddeaef15600
Crowdbotics <team@crowdbotics.com>
2021-09-13 17:58:30 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "mytrueweight_30515Identifier",
+  url: "https://mytrueweight_30515Identifier.botics.co",
+  api: "https://mytrueweight_30515Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
